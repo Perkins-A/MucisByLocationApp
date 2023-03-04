7a9099123034b492dd0c3e0047f7a2e0977b2c6f
Alex Perkins <perkins.a@etoncollege.org.uk>
2023-03-04 08:03:32 +0000

    Initial Commit

---
+++ MucisByLocationApp/MucisByLocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MucisByLocationAppApp.swift
+//  MucisByLocationApp
+//
+//  Created by Perkins, Alexander (ABH) on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MucisByLocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
